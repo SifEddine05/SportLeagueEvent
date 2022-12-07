b622775e739cf1bbf41172f8d9ce1ea90d3bc787
AhmedSEHILI <ka_sehili@esi.dz>
2022-12-07 17:51:32 +0100

    Hero fixes

--- comps/Nav.js
+++ comps/Nav.js
@@ -3,16 +3,16 @@ import Image
  from 'next/image';
 function Nav(props) {
     return (
-        <div className=" w-screen flex flex-row justify-between mt-3 pb-3">
+        <div className=" w-screen flex flex-row justify-between mt-3 pb-2 lg:pb-3">
             <Image src ="/logo.svg" width={50} height={50} className="ml-8"/>
-            <div className="flex flex-row gap-6 mr-12 mt-3">
-                <div className="px-3 py-1 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl">Home</div>
-                <div className="px-3 py-1 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl">About</div>
-                <div className="px-3 py-1 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl">Sponsors</div>
-                <div className="px-3 py-1 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl">Speakers</div>
-                <div className="px-3 py-1 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl">FAQ</div>
-                <div className="px-3 py-1 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl">Contact Us</div>
-                <div className="px-3 py-1 font-semibold ml-12 border-2 hover:font-bold hover:bg-[#1E96FC] hover:text-white border-slate-500 rounded-2xl">Sign Up</div>
+            <div className="flex flex-row gap-2 lg:gap-6 mr-12 mt-3">
+                <div className="px-3 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl text-sm lg:text-lg">Home</div>
+                <div className="px-3 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl text-sm lg:text-lg">About</div>
+                <div className="px-3 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl text-sm lg:text-lg">Sponsors</div>
+                <div className="px-3 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl text-sm lg:text-lg">Speakers</div>
+                <div className="px-3 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl text-sm lg:text-lg">FAQ</div>
+                <div className="px-3 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl text-sm lg:text-lg">Contact Us</div>
+                <div className="px-3 lg:py-1 py-2 font-semibold ml-12 border-2 hover:font-bold hover:bg-[#1E96FC] hover:text-white border-slate-500 rounded-2xl text-sm lg:text-lg">Sign Up</div>
             </div>
                 
         </div>
