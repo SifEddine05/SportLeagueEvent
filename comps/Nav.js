7453a8fda3051c1cef64d9c43ea11805fa99d8f5
AhmedSEHILI <ka_sehili@esi.dz>
2022-12-09 16:49:52 +0100

    404 fixed

--- comps/Nav.js
+++ comps/Nav.js
@@ -46,13 +46,13 @@ const handlClick =()=>{
                     <Link href="#footer">
                         <div className="cursor-pointer md:px-3 px-2 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl md:text-md sm:text-sm text-[9px] lg:text-lg">Contact Us</div>
                     </Link>
-                    
-                    
-                    <div className="cursor-pointer md:px-3 px-1 lg:py-1 py-2 font-semibold lg:ml-12 md:ml-9 sm:ml-6 ml-3 border-2 hover:font-bold hover:bg-[#1E96FC] hover:text-white border-slate-500 rounded-2xl md:text-md sm:text-sm text-[8px] lg:text-lg">Sign Up</div>
+                    <Link href="error">
+                        <div className="cursor-pointer md:px-3 px-1 lg:py-1 py-2 font-semibold lg:ml-12 md:ml-9 sm:ml-6 ml-3 border-2 hover:font-bold hover:bg-[#1E96FC] hover:text-white border-slate-500 rounded-2xl md:text-md sm:text-sm text-[8px] lg:text-lg">Sign Up</div>
+                    </Link>
                 </div>
 
             </div>
-            <div className=' flex justify-start sm:hidden inline'>
+            <div className=' flex justify-start sm:hidden'>
             {style&& <div className='sm:hidden inline'>
                 <div className="flex   flex-row justify-center items-center gap-1 md:gap-3 sm:gap-2 lg:gap-5 pt-2 lg:mr-12 md:mr-9 sm:mr-7 mr-5  mt-3">
                 <Link href="/">
@@ -76,9 +76,10 @@ const handlClick =()=>{
                     <Link href="#footer">
                         <div className="cursor-pointer md:px-3 px-1 lg:py-1 py-2 font-semibold hover:border-2 hover:font-bold border-slate-500 hover:rounded-2xl md:text-md sm:text-sm text-[9px] text-centerlg:text-lg">Contact Us</div>
                     </Link>
+                    <Link href="errr">
+                        <div className="cursor-pointer md:px-3 px-1 lg:py-1 py-2 font-semibold lg:ml-12 md:ml-9 sm:ml-6 ml-1 border-2 hover:font-bold hover:bg-[#1E96FC] hover:text-white border-slate-500 rounded-2xl md:text-md sm:text-sm text-[9px] text-center lg:text-lg">Sign Up</div>
+                    </Link>
                     
-                    
-                    <div className="cursor-pointer md:px-3 px-1 lg:py-1 py-2 font-semibold lg:ml-12 md:ml-9 sm:ml-6 ml-1 border-2 hover:font-bold hover:bg-[#1E96FC] hover:text-white border-slate-500 rounded-2xl md:text-md sm:text-sm text-[9px] text-center lg:text-lg">Sign Up</div>
                 </div>
             </div> }
             <button className=' ml-1   sm:hidden inline ' onClick={handlClick}  ><img src="/Menu1.png" alt="menu icon "  className='w-[25px] mr-4 pt-2' /></button>
