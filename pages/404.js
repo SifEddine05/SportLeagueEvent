daff01b232ad9c3b8614546e29a3e98888323f32
AhmedSEHILI <ka_sehili@esi.dz>
2022-12-09 16:28:22 +0100

    fixes

--- pages/404.js
+++ pages/404.js
@@ -16,12 +16,12 @@ function Notfound(props) {
     })
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen w-screen bg-black">
+        <div className="flex flex-col justify-center items-center h-screen w-screen bg-[#01032A]">
             <h3 className="font-bold text-white">This page is not ready</h3>
             <h3 className="font-bold text-white">You will be redirected to the main page in {count} seconds</h3>
             <h3 className="py-7 text-sm text-white">with best regards / ESMS team 17 </h3>
             <div className="w-[5%] h-[5%]">
-                                <img src="/cse.svg" alt="" />
+            <img src="/cse.svg" alt="" />
             </div>
 
         </div>
