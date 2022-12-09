f4d5fa4ed0ca12eb74b165f80b459345653b3824
AhmedSEHILI <ka_sehili@esi.dz>
2022-12-09 02:21:03 +0100

    Changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import AgEv from '../comps/AgEv'
 import Nav from '../comps/Nav'
-import styles from '../styles/Home.module.css'
 import About from './About'
 import Faq from './FAQ'
 import Footer from './footer'
@@ -10,7 +7,6 @@ import Agenda from './Agenda'
 import Hm from './Hm'
 import Sponsors from './Sponsors'
 import Speakers from '../comps/sections/speakers'
-import Layout from '../comps/layout'
 
 
 
